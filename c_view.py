27e527c200acd0f9a485946e2219ce0cfca5f1b9
Majkels92 <83303248+Majkels92@users.noreply.github.com>
2021-05-08 09:04:54 +0200

    Add files via upload

--- c_view.py
+++ c_view.py
@@ -6,35 +6,31 @@ status = []
 checkbuttons = []
 textlist = []
 
-
 def config():
-    global listback, okno_glowne, button_display_list, button_add_task, button_delete_task, button_delete_all_done, button_exit_app
-    okno_glowne = Tk()
-    okno_glowne.title('Lista zadan')
-    okno_glowne.geometry("500x500")
-    button_display_list = Button(okno_glowne, text='Display Tasks', width=12, command=display)
+    global main_root, button_display_list, button_add_task, button_delete_task, button_delete_all_done, button_exit_app
+    main_root = Tk()
+    main_root.title('Lista zadan')
+    main_root.geometry("500x500")
+    button_display_list = Button(main_root, text='Display Tasks', width=12, command=display)
     button_display_list.grid(column=0, row=4)
-    button_add_task = Button(okno_glowne, text='Add Task', width=12, command=add)
+    button_add_task = Button(main_root, text='Add Task', width=12, command=add)
     button_add_task.grid(column=0, row=0)
-    button_delete_task = Button(okno_glowne, text='Delete Task', width=12)
+    button_delete_task = Button(main_root, text='Delete Task', width=12)
     button_delete_task.grid(column=0, row=1)
-    button_delete_all_done = Button(okno_glowne, text='Delete All Done', width=12)
+    button_delete_all_done = Button(main_root, text='Delete All Done', width=12)
     button_delete_all_done.grid(column=0, row=2)
-    button_exit_app = Button(okno_glowne, text='EXIT', width=12, command=exit_window)
+    button_exit_app = Button(main_root, text='EXIT', width=12, command=exit_window)
     button_exit_app.grid(column=0, row=3)
 
-
 def exit_window():
-    okno_glowne.destroy()
-
+    main_root.destroy()
 
 def add():
     global task, button_add
-    insert_root = Tk()
+    insert_root = Toplevel()
     insert_root.title('Dodaj zadanie:')
     task = Entry(insert_root, width=80)
     task.grid(column=0, row=0, padx=20, pady=20)
-
     def add_task():
         global task_obj, calendar
         date = c_body.CalendarObj.nowDate()
@@ -42,11 +38,9 @@ def add():
         task_obj = c_body.CalendarObj(task.get(), date, time)
         calendar.append(task_obj)
         insert_root.destroy()
-
     button_add = Button(insert_root, text='dodaj zadanie', command=add_task)
     button_add.grid(column=0, row=1, sticky=E, padx=20, pady=20)
     insert_root.mainloop()
-
 
 def callback_on_checkbutton_click():
     print("One of the Checkbuttons clicked!")
@@ -55,13 +49,13 @@ def callback_on_checkbutton_click():
         calendar[i].stat = status[i].get()
         print('\t\tNew calendar[' + str(i) + '] state: ' + calendar[i].displayObj())
 
-
 def display():
     for i in range(len(calendar)):
         lp = i + 1
         print("status dla:", lp, calendar[i].stat)
         x = (str(lp) + '. ' + calendar[i].displayObj())
-        textlist.append(x)
+        textlist.append(StringVar())
+        textlist[i].set(x)
         status.append(BooleanVar())
         if calendar[i].stat == False:
             print("display() called. For index i: ", i, ' setting checkbutton value to checked (onvalue=0)')
@@ -71,20 +65,21 @@ def display():
             status[i].set(True)
         checkbuttons.append(
             Checkbutton(
-                master=okno_glowne,
-                text=textlist[i],
+                master=main_root,
+                textvariable=textlist[i],
                 variable=status[i],
                 onvalue=True,
                 offvalue=False,
                 command=callback_on_checkbutton_click
             )
         )
+        checkbuttons[i].grid(column=1, row=i+1, sticky=W)
 
-        checkbuttons[i].grid(column=1, row=i, sticky=W)
+if __name__ == '__main__':
+    config()
+    main_root.mainloop()
 
 
-config()
-okno_glowne.mainloop()
 """def delete():
     lp = input('delete task number: ')
     i = int(lp) - 1
