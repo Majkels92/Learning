438d66d4e5c5c46b41feecb3f81d0d0f138d1cf2
Majkels92 <83303248+Majkels92@users.noreply.github.com>
2021-05-11 15:40:11 +0200

    Add files via upload

--- c_view.py
+++ c_view.py
@@ -5,23 +5,23 @@ calendar = []
 status = []
 checkbuttons = []
 textlist = []
-
 def config():
-    global main_root, button_display_list, button_add_task, button_delete_task, button_delete_all_done, button_exit_app
+    global button_frame, main_root, button_display_list, button_add_task, button_delete_task, button_delete_all_done, button_exit_app, check_frame
     main_root = Tk()
     main_root.title('Lista zadan')
     main_root.geometry("500x500")
-    button_display_list = Button(main_root, text='Display Tasks', width=12, command=display)
-    button_display_list.grid(column=0, row=4)
-    button_add_task = Button(main_root, text='Add Task', width=12, command=add)
+    button_frame = Frame(main_root)
+    button_frame.grid(column=0, row=1)
+    button_add_task = Button(button_frame, text='Add Task', width=12, command=add)
     button_add_task.grid(column=0, row=0)
-    button_delete_task = Button(main_root, text='Delete Task', width=12, command=delete)
+    button_delete_task = Button(button_frame, text='Delete Task', width=12, command=delete)
     button_delete_task.grid(column=0, row=1)
-    button_delete_all_done = Button(main_root, text='Delete All Done', width=12, command=del_all_done)
+    button_delete_all_done = Button(button_frame, text='Delete All Done', width=12, command=del_all_done)
     button_delete_all_done.grid(column=0, row=2)
-    button_exit_app = Button(main_root, text='EXIT', width=12, command=exit_window)
+    button_exit_app = Button(button_frame, text='EXIT', width=12, command=exit_window)
     button_exit_app.grid(column=0, row=3)
-
+    check_frame = Frame(main_root)
+    check_frame.grid(column=1, row=1)
 def add():
     insert_root = Toplevel()
     insert_root.title('Dodaj zadanie:')
@@ -40,27 +40,103 @@ def add():
     button_add = Button(insert_root, text='dodaj zadanie', command=add_task)
     button_add.grid(column=0, row=1, sticky=E, padx=20, pady=20)
     insert_root.mainloop()
-
 def delete():
-    delete_root = Toplevel()
-    delete_root.title('Delete task:')
-    deltask = Entry(delete_root, width=80)
-    deltask.grid(column=0, row=0, padx=20, pady=20)
-    def del_task():
-        global task_obj, calendar
-        i = int(deltask.get()) - 1
-        calendar.remove(calendar[i])
-        status.remove(status[i])
-        checkbuttons[i].destroy()
-        checkbuttons.remove(checkbuttons[i])
-        textlist.remove(textlist[i])
-        display()
-        delete_root.destroy()
-    button_add = Button(delete_root, text='delete task', command=del_task)
-    button_add.grid(column=0, row=1, sticky=E, padx=20, pady=20)
-    delete_root.mainloop()
-
+    if len(calendar) > 0 :
+        delete_root = Toplevel()
+        delete_root.title('Delete task:')
+        deltask = Entry(delete_root, width=80)
+        deltask.grid(column=0, row=0, padx=20, pady=20)
+        def del_task():
+            i = int(deltask.get()) - int(1)
+            status.remove(status[i])
+            textlist.remove(textlist[i])
+            calendar.remove(calendar[i])
+            display()
+            delete_root.destroy()
+        button_add = Button(delete_root, text='delete task', command=del_task)
+        button_add.grid(column=0, row=1, sticky=E, padx=20, pady=20)
+        delete_root.mainloop()
+    elif len(calendar) == 0:
+        delete_info_root = Toplevel()
+        delete_info_root.geometry("300x100")
+        delete_info_root.title('Error information:')
+        text_info = Label(delete_info_root, text = "YOU SHALL NOT PASS,\n YOU CAN'T DELETE ANYTHING FROM EMPTY LIST!!")
+        text_info.pack()
+        def exit_root():
+            delete_info_root.destroy()
+        button_ok = Button(delete_info_root, text='OK, SORRY', command=exit_root)
+        button_ok.pack()
+        delete_info_root.mainloop()
 def del_all_done():
+    def check_if_true():
+        for i in range(len(calendar)):
+            if calendar[i].stat == True:
+                return True
+    if check_if_true() == True:
+        global calendar, status, textlist
+        bufor_calendar = []
+        bufor_status = []
+        bufor_textlist = []
+        for i in range(len(calendar)):
+            if calendar[i].stat == False:
+                bufor_calendar.append(calendar[i])
+                bufor_status.append(status[i])
+                bufor_textlist.append(textlist[i])
+        calendar = bufor_calendar
+        status = bufor_status
+        textlist = bufor_textlist
+        display()
+
+    else:
+        delete_info_root = Toplevel()
+        delete_info_root.geometry("300x100")
+        delete_info_root.title('Error information:')
+        text_info = Label(delete_info_root, text="YOU SHALL NOT PASS,\n YOU CAN'T DELETE ANYTHING WHEN YOU HAVE WORK TO DO!!!!!!")
+        text_info.pack()
+
+        def exit_root():
+            delete_info_root.destroy()
+
+        button_ok = Button(delete_info_root, text='OK, SORRY', command=exit_root)
+        button_ok.pack()
+        delete_info_root.mainloop()
+def display():
+    global calendar, textlist, status, checkbuttons, check_frame
+    def callback_on_checkbutton_click():
+        for i in range(len(calendar)):
+            calendar[i].stat = status[i].get()
+    checkbuttons.clear()
+    check_frame.destroy()
+    check_frame = Frame(main_root)
+    check_frame.grid(column=1, row=1)
+    for i in range(len(calendar)):
+        lp = i + 1
+        x = (str(lp) + '. ' + calendar[i].displayObj())
+        textlist[i].set(x)
+        if calendar[i].stat == False:
+            status[i].set(False)
+        elif calendar[i].stat == True:
+            status[i].set(True)
+        checkbuttons.append(
+            Checkbutton(
+                master=check_frame,
+                textvariable=textlist[i],
+                variable=status[i],
+                onvalue=True,
+                offvalue=False,
+                command=callback_on_checkbutton_click
+            )
+        )
+        checkbuttons[i].grid(column=1, row=i, sticky=W)
+
+def exit_window():
+    main_root.destroy()
+if __name__ == '__main__':
+    config()
+    main_root.mainloop()
+
+
+"""def del_all_done():
     global calendar, status, checkbuttons, textlist
     bufor_calendar = []
     bufor_status = []
@@ -78,48 +154,6 @@ def del_all_done():
     checkbuttons = bufor_checkbuttons
     textlist = bufor_textlist
     return calendar, status, checkbuttons, textlist
-
-def display():
-    global calendar, textlist, status, checkbuttons
-    def callback_on_checkbutton_click():
-        print("One of the Checkbuttons clicked!")
-        for i in range(len(calendar)):
-            print('\tOld calendar[' + str(i) + '] state: ' + calendar[i].displayObj())
-            calendar[i].stat = status[i].get()
-            print('\t\tNew calendar[' + str(i) + '] state: ' + calendar[i].displayObj())
-    checkbuttons.clear()
-    for i in range(len(calendar)):
-        lp = i + 1
-        print("status dla:", lp, calendar[i].stat)
-        x = (str(lp) + '. ' + calendar[i].displayObj())
-        textlist[i].set(x)
-        if calendar[i].stat == False:
-            print("display() called. For index i: ", i, ' setting checkbutton value to checked (onvalue=0)')
-            status[i].set(False)
-        elif calendar[i].stat == True:
-            print("display() called. For index i: ", i, ' setting checkbutton value to unchecked (offvalue=1)')
-            status[i].set(True)
-        checkbuttons.append(
-            Checkbutton(
-                master=main_root,
-                textvariable=textlist[i],
-                variable=status[i],
-                onvalue=True,
-                offvalue=False,
-                command=callback_on_checkbutton_click
-            )
-        )
-        checkbuttons[i].grid(column=1, row=i+1, sticky=W)
-
-def exit_window():
-    main_root.destroy()
-
-if __name__ == '__main__':
-    config()
-    main_root.mainloop()
-
-
-"""
     """
 """def del_all_done():
     global calendar
@@ -145,11 +179,5 @@ def del_all_done():
             bufor_list.append(i)
     calendar = bufor_list
     return calendar
-    
-    def callback_on_checkbutton_click():
-    print("One of the Checkbuttons clicked!")
-    for i in range(len(calendar)):
-        print('\tOld calendar[' + str(i) + '] state: ' + calendar[i].displayObj())
-        calendar[i].stat = status[i].get() == 1
-        print('\t\tNew calendar[' + str(i) + '] state: ' + calendar[i].displayObj())
+
     """
