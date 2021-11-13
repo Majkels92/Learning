701cf8604e62366e61604d052936966cd7c5ca47
Majkels92 <michal.skowr92@gmail.com>
2021-11-13 11:34:51 +0100

    Add functions, possibility to save prices of crypto in .txt

--- crypto/stock_crypto.py
+++ crypto/stock_crypto.py
@@ -1,29 +1,44 @@
 import requests
-import pprint
+import datetime
 
-cryptos = {
-    'bitcoin': 'Qwsogvtv82FCd',
-    'etherum': 'razxDUgYGNAdQ',
-    'dogecoin': 'a91GCGd_u96cF',
-    'shibainu': 'xz24e0BjL',
-    'litecoin': 'D7B1x_ks7WhV5',
+
+def downloading_data():
+    global response, chosen_crypto_price, chosen_crypto_name, chosen_crypto_symbol
+    cryptos = {
+        'bitcoin': 'Qwsogvtv82FCd',
+        'etherum': 'razxDUgYGNAdQ',
+        'dogecoin': 'a91GCGd_u96cF',
+        'shibainu': 'xz24e0BjL',
+        'litecoin': 'D7B1x_ks7WhV5',
     }
 
-crypto_input = input("enter the name of crypto:")
-if crypto_input in cryptos:
-    choice = cryptos[crypto_input]
-else:
-    print("Sorry we do not have that crypto")
+    crypto_input = input("enter the name of crypto which You want to choose: ")
+    if crypto_input in cryptos:
+        choice = cryptos[crypto_input]
+    else:
+        print("Sorry we do not have that crypto")
 
-url = f"https://api.coinranking.com/v2/coin/{choice}"
+    url = f"https://api.coinranking.com/v2/coin/{choice}"
 
-headers = {
-    "x-access-token": "coinrankingd8901efc7e4b47aafe481b49cf5deed5d7dd3e8b27d1c6a1"
+    headers = {
+        "x-access-token": "coinrankingd8901efc7e4b47aafe481b49cf5deed5d7dd3e8b27d1c6a1"
     }
 
-r = requests.get(url, headers=headers)
-response = r.json()["data"]["coin"]
+    r = requests.get(url, headers=headers)
+    response = r.json()["data"]["coin"]
 
-pprint.pprint(response["name"])
-pprint.pprint(response["symbol"])
-pprint.pprint(response["price"])
+    chosen_crypto_symbol = response["symbol"]
+    chosen_crypto_name = response["name"]
+    chosen_crypto_price = response["price"]
+    return response, chosen_crypto_symbol, chosen_crypto_name, chosen_crypto_price
+
+def crypto_data_log():
+    with open("crypto price.txt", "a") as file:
+        response_time = None
+        file.write(f"{chosen_crypto_symbol}: {chosen_crypto_name} {float(chosen_crypto_price):.5f} [USD]\n")
+
+if __name__ == '__main__':
+    downloading_data()
+    crypto_data_log()
+
+    input('Press ENTER to exit')
