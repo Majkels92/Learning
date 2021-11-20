c69ec862c8742a339a08df0fe068ecfc8063d45b
Majkels92 <michal.skowr92@gmail.com>
2021-11-20 16:13:20 +0100

    Cosmetics, finish menu.

--- crypto/stock_crypto.py
+++ crypto/stock_crypto.py
@@ -2,7 +2,13 @@ import requests
 import datetime
 
 
+def help_available_cryptos():
+    """Informs user about avaible cryptos."""
+    print("""bitcoin\netherum\ndogecoin\nshibainu\nlitecoin""")
+
+
 def downloading_data():
+    """Downloads from server chosen crypto data."""
     global response, chosen_crypto_price, chosen_crypto_name, chosen_crypto_symbol
     cryptos = {
         'bitcoin': 'Qwsogvtv82FCd',
@@ -30,59 +36,78 @@ def downloading_data():
     chosen_crypto_symbol = response["symbol"]
     chosen_crypto_name = response["name"]
     chosen_crypto_price = response["price"]
-    return response, chosen_crypto_symbol, chosen_crypto_name, chosen_crypto_price
+
+
+def response_time_from_server():
+    """Return time of function call out, function used for determine time of action in App.
+    The time for is delivered as: [13.11.2021r. 12:09:05][day.month.year hour.minute.second]"""
+    time_log = datetime.datetime.now().strftime("%d.%m.%Yr. %H:%M:%S")
+    return time_log
 
 
 def appending_sought_item_to_list():
+    global price_list
+    """Appends searched price of crypto to list as string."""
+    response_time = response_time_from_server()
     item = f"[{response_time}] {chosen_crypto_symbol}: {chosen_crypto_name} {float(chosen_crypto_price):.5f} "\
-           f"USD\n "
+           f"USD"
     price_list.append(item)
-    return price_list
 
 
-def print_price():
+def check_n_print_price():
+    """Checks price of chosen crypto and appends that price to list to list"""
     downloading_data()
     print(f"Price of {chosen_crypto_name} is {float(chosen_crypto_price):.5f} [USD]")
     appending_sought_item_to_list()
 
 
-def available_cryptos():
-    print("""bitcoin\netherum\ndogecoin\nshibainu\nlitecoin""")
-
-
 def single_crypto_data_log():
-    global response_time
+    """Appends or creates file crypto log.txt with history of checked prices, represented as every record in new line.
+    Every line contains time:crypto symbol:crypto name:crypto price with 5 numbers after decimal."""
     with open("crypto log.txt", "a") as file:
-        response_time = datetime.datetime.now().strftime("%d.%m.%Yr. %H:%M:%S")
+        response_time = response_time_from_server()
         file.write(f"[{response_time}] {chosen_crypto_symbol}: {chosen_crypto_name} {float(chosen_crypto_price):.5f} "
                    f"USD\n ")
 
 
 def sought_crypto_data_log():
+    """Transfers elements from list to txt file, after transfer clears list."""
     with open("crypto log.txt", "a") as file:
-        file.writelines(price_list)
+        for item in price_list:
+            file.write(item+"\n")
+    price_list.clear()
 
 
 def menu():
-    global price_list
-    price_list = []
-    action = input("""what u want to do?")
-    - """)
+    """Creates menu for App, function that controls App."""
+    action = input("""I want to: """)
     if action == "check price":
-        print_price()
+        check_n_print_price()
+        menu()
     elif action == "create log":
-
-        print("\nFile with logs created.\n")
-    elif action == "save sought scores":
+        single_crypto_data_log()
+        print("\nFile with logs created/updated.\n")
+        menu()
+    elif action == "save scores":
         sought_crypto_data_log()
+        menu()
+    elif action == "exit":
+        exit()
+    elif action == "help":
+        help_available_cryptos()
+        menu()
     else:
         print("Unknown action.")
         menu()
 
+price_list = []
 
 if __name__ == '__main__':
-    print("     Welcome to my CRYPTO App! :)")
-    downloading_data()
-
-
-    input('Press ENTER to exit')
+    print("     Welcome to my CRYPTO App! :)"
+    """what u want to do?")
+        - check price of crypto type: check price
+        - to check available cryptos type: help
+        - create or append data of crypto price do file type: create log
+        - save sought scores during running app and add to txt file, type: save scores
+        - to exit app type: exit""")
+    menu()
