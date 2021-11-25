3f625d81718b7761c472bac71b816eb0b3f1aeaa
Majkels92 <michal.skowr92@gmail.com>
2021-11-25 19:25:25 +0100

    Adds Weapons class.

--- monster and swords game/monster.py
+++ monster and swords game/monster.py
@@ -1,4 +1,7 @@
+import random
+
 class Creature:
+    """Defines basic statistics of every creature and person in game."""
 
     def __init__(self, hp = 100, mp = 100, spd = 1):
         self.set_hp(hp)
@@ -23,10 +26,29 @@ class Creature:
         else:
             raise TypeError("Attribute must be integer and greater than 0.")
 
-    def show_stats(self):
+    def show_creature_stats(self):
         print(f"This subject has: \n{self.health_points} hp \n{self.mana_points} mp \n{self.speed} speed")
 
+class Weapons:
+
+    def __init__(self):
+        self.set_damage()
+        self.set_attack_speed()
+
+    def set_damage(self):
+        self.damage = random.randint(1, 100)
+
+    def set_attack_speed(self):
+        self.attack_speed = (random.randrange(10, 101))/10
+
+    def show_weapon_stats(self):
+        print(f"This weapon has: \n{self.damage} dmg\n{self.attack_speed} att spd")
 
 a = Creature(200, 300, 4)
-a.show_stats()
+a.show_creature_stats()
+b = Weapons()
+b.show_weapon_stats()
+
+
+
 
