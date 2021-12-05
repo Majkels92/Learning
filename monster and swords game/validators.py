19dd8fd4ce1abbb9a1c8725dd2c0cc944db33154
Majkels92 <michal.skowr92@gmail.com>
2021-12-05 12:00:38 +0100

    Adds 2 validator (string and integer).

---
+++ monster and swords game/validators.py
@@ -0,0 +1,21 @@
+"""Python file including functions that validates input data in monster.py file/app."""
+
+
+# validate input value, value must be INT type and value > 0
+def validate_int_value(int_value):
+    if isinstance(int_value, int) and int_value > 0:
+        return int_value
+    else:
+        if not isinstance(int_value, int):
+            raise TypeError("Attribute must be integer type.")
+        elif int_value <= 0:
+            raise ValueError("Attribute must be greater than 0.")
+
+
+# validate input value, value must be STR type
+def validate_str_value(str_value):
+    if isinstance(str_value, str):
+        return str_value
+    else:
+        raise TypeError("Attribute must be string type.")
+
