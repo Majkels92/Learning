4fe0aefd8fdec457fd119e1d2e9a362673f9ee90
Majkels92 <michal.skowr92@gmail.com>
2021-12-05 12:56:18 +0100

    Adds weighted possibility in function that draws attack speed of weapon.

--- monster and swords game/monster.py
+++ monster and swords game/monster.py
@@ -11,7 +11,7 @@ class Creature:
         self._speed = validators.validate_int_value(spd)
         self._name = validators.validate_str_value(name)
 
-    # show objects: health points, mana points and speed
+    # show instance: health points, mana points and speed
     def show_creature_stats(self):
         print(f"{self._name} has: \n{self._health_points} hp \n{self._mana_points} mp \n{self._speed} speed")
 
@@ -20,18 +20,29 @@ class Weapons:
     """Defines damage and attack speed of weapon"""
 
     def __init__(self):
-        self.set_damage()
-        self.set_attack_speed()
+        self._basic_damage = random.randint(1, 100)
+        self._basic_attack_speed = Weapons.setting_att_speed()
 
-    def set_damage(self):
-        self.damage = random.randint(1, 100)
+    # draw value of attack speed with weighted possibility (common = 60%, rare = 30%, legendary=10%)
+    # att_spd_range - attribute used for increasing possibility of better attack speed draw, used in Chest class
+    @staticmethod
+    def setting_att_speed(att_spd_range=1):
+        """ Draw value of attack speed with weighted possibility (common = 60%, rare = 30%, legendary=10%);
+        att_spd_range - attribute used for increasing possibility of better attack speed draw, used in Chest class"""
+        possibility = random.randint(att_spd_range, 10)
+        if possibility <= 6:
+            return (random.randrange(10, 51))/10
+        elif possibility <= 9:
+            return (random.randrange(50, 81))/10
+        else:
+            return (random.randrange(80, 101))/10
 
-    def set_attack_speed(self):
-        self.attack_speed = (random.randrange(10, 101))/10
-
+    # show instance: basic damage and attack speed
     def show_weapon_stats(self):
-        print(f"This weapon has: \n{self.damage} dmg\n{self.attack_speed} att spd")
+        print(f"This weapon has: \n{self._basic_damage} dmg\n{self._basic_attack_speed} att spd")
 
 
 a = Creature(200, 300, 4)
 a.show_creature_stats()
+w = Weapons()
+w.show_weapon_stats()
