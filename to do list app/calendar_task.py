fc5ef114b6a2f684a7d215002b6f392479c12690
Majkels92 <michal.skowr92@gmail.com>
2021-12-06 20:34:47 +0100

    Cosmetics.

--- to do list app/calendar_task.py
+++ to do list app/calendar_task.py
@@ -1,17 +1,23 @@
 import datetime
 
+
 class CalendarTask:
-    def __init__(self, task, date, time, stat = False):
+
+    def __init__(self, task, date, time, stat=False):
         self.task = task
         self.stat = stat
         self.date = date
         self.time = time
+
     def displayObj(self):
-        return self.task + ' ' +  self.date  + ' ' + self.time
+        return self.task + ' ' + self.date + ' ' + self.time
+
+    @staticmethod
     def nowDate():
         date = datetime.datetime.today().strftime("%d %B")
         return date
+
+    @staticmethod
     def nowTime():
         time = datetime.datetime.today().strftime("%H:%M")
         return time
-
