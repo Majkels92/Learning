cceff70da3781fcf3fa2217ca0ec17347c58bf32
Majkels92 <michal.skowr92@gmail.com>
2021-12-08 21:59:53 +0100

    Create Chest methods.

--- monster and swords game/monster.py
+++ monster and swords game/monster.py
@@ -1,3 +1,4 @@
+"""File containing code defining game content such as characters, items etc...."""
 import random
 import validators
 
@@ -5,15 +6,16 @@ import validators
 class Creature:
     """Defines basic statistics of every creature and person in game."""
 
-    def __init__(self, hp=100, mp=100, spd=1, name="John Doe"):
+    def __init__(self, hp=100, mp=100, m_spd=1, name="John Doe"):
         self._health_points = validators.validate_int_value(hp)
         self._mana_points = validators.validate_int_value(mp)
-        self._speed = validators.validate_int_value(spd)
+        self._movement_speed = validators.validate_int_value(m_spd)
         self._name = validators.validate_str_value(name)
 
     # show instance: health points, mana points and speed
     def show_creature_stats(self):
-        print(f"{self._name} has: \n{self._health_points} hp \n{self._mana_points} mp \n{self._speed} speed")
+        print(f"{self._name} has: \n{self._health_points} hp \n{self._mana_points} mp \n{self._movement_speed} "
+              f"movement speed")
 
 
 class Weapons:
@@ -26,7 +28,7 @@ class Weapons:
     # draw value of basic attack speed with weighted possibility
     @staticmethod
     def setting_att_speed(att_spd_range=1):
-        """ Draw value of basic attack speed with weighted possibility (common = 60%, rare = 30%, legendary=10%);
+        """ Draw value of basic attack speed with weighted possibility (common = 60%, rare = 30%, legendary=10%)
         att_spd_range - attribute used for increasing possibility of better attack speed draw, used in Chest class"""
         possibility = random.randint(att_spd_range, 10)
         if possibility <= 6:
@@ -40,6 +42,10 @@ class Weapons:
     @staticmethod
     def setting_dmg(dmg_range=1):
         """ Draw value of basic damage with weighted possibility (common = 60%, rare = 30%, legendary=10%);
+
+        setting_dmg(dmg_range)
+        dmg_range: default value = 1
+
         dmg_range - attribute used for increasing possibility of better attack speed draw, used in Chest class"""
         possibility = random.randint(dmg_range, 10)
         if possibility <= 6:
@@ -52,6 +58,41 @@ class Weapons:
     # show instance: basic damage and attack speed
     def show_weapon_stats(self):
         print(f"This weapon has: \n{self._basic_damage} dmg\n{self._basic_attack_speed} att spd")
+
+
+class Chest:
+
+    def __init__(self):
+        self.rarity = Chest.draw_rarity()
+        self.drop = self.chest_gold_drop()
+
+    # draws and set rarity of chest
+    @staticmethod
+    def draw_rarity():
+        chest_rarity = random.randint(1, 10)
+        if chest_rarity <= 6:
+            return "wooden"
+        elif chest_rarity <= 9:
+            return "iron"
+        else:
+            return "golden"
+
+    # draws amount of gold dropped from chest
+    def chest_gold_drop(self):
+        if self.rarity == "golden":
+            drop = 100
+        elif self.rarity == "iron":
+            drop = 50
+        elif self.rarity == "wooden":
+            drop = 10
+        return drop
+
+
+
+
+
+class ItemContainers:
+    pass
 
 
 a = Creature(200, 300, 4)
