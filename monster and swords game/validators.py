9eaf6d314e48446237d89f00bf18554396f40075
Majkels92 <michal.skowr92@gmail.com>
2021-12-13 21:56:25 +0100

    changes name of validator

--- monster and swords game/validators.py
+++ monster and swords game/validators.py
@@ -3,6 +3,7 @@
 
 # validate input value, value must be INT type and value > 0
 def validate_int_value(int_value):
+    """Must be INT type and value > 0"""
     if isinstance(int_value, int) and int_value > 0:
         return int_value
     else:
@@ -12,7 +13,8 @@ def validate_int_value(int_value):
             raise ValueError("Attribute must be greater than 0.")
 
 # validates input of gold sack, value must be INT and value >=0
-def validate_sack_value(int_value):
+def validate_int_value_2 (int_value):
+    """Must be INT and value >=0"""
     if isinstance(int_value, int) and int_value >= 0:
         return int_value
     else:
