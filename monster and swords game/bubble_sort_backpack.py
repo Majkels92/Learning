173a49da88053393cffec6ef9065eafc93858bbb
Majkels92 <michal.skowr92@gmail.com>
2022-01-08 18:18:32 +0100

    Adds file with bubble sort of backpack slots (deletes "Empty slots" between items

---
+++ monster and swords game/bubble_sort_backpack.py
@@ -0,0 +1,17 @@
+lista = [1, 2, "Empty slot", 5, "Empty slot", "Empty slot", 6]
+
+def bubble_sorting(sorted_list):
+    for i in range(len(sorted_list)-1):
+        if sorted_list[i] == "Empty slot":
+            if sorted_list[i+1] != "Empty slot":
+                sorted_list[i], sorted_list[i+1] = sorted_list[i+1], sorted_list[i]
+            elif sorted_list[i+1] == "Empty slot":
+                k = 1
+                while i+k+1 != len(sorted_list):
+                    if sorted_list[i + 1 + k] != "Empty slot":
+                        sorted_list[i], sorted_list[i+1+k] = sorted_list[i+1+k], sorted_list[i]
+                    k += 1
+    return sorted_list
+
+print(bubble_sorting(lista))
+
